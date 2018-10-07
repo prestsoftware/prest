2422d008a40be81f0865d807a664d4ce0514d827
Matus Tejiscak <ziman@functor.sk>
2018-10-07 10:49:44 +0200

    Add favicon.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -115,6 +115,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_favicon = '_static/favicon.png'
 
 
 #TEXT-WIDTH CUSTOMISATION

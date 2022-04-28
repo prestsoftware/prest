4b367e045e1d94e5515a9ebb052fc15d92b71a67
Matus Tejiscak <ziman@functor.sk>
2022-04-28 22:16:46 +0200

    Hide the navbar.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -196,7 +196,7 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_favicon = '_static/favicon.png'
-
+html_css_files = ['my_theme.css']
 
 #TEXT-WIDTH CUSTOMISATION
 #def setup(app):

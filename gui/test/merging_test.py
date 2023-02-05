27e0b391465f4ea08ba85ea9e0bb0a3cbc519fa7
Matus Tejiscak <ziman@functor.sk>
2023-02-05 21:57:04 +0100

    Add more tests for merging with no deferrals.

--- gui/test/merging_test.py
+++ gui/test/merging_test.py
@@ -55,3 +55,56 @@ def test_merging_default():
         ('subjA', 'Ca,Hi,Pa', 'Hi', ''),
         None,  # bump progress
     ]
+
+def test_merging_nodef():
+    rows = [
+        'subjA Ca,Hi,Pa Ca',
+        'subjA Ca,Pa Pa',
+        'subjA Ca,Hi,Pa Pa',
+        'subjA Ca Ca',
+        'subjA Ca,Hi,Pa ',
+        'subjA Ca,Pa Ca',
+        'subjA Ca,Pa,Hi Hi',
+    ]
+    ds = ExperimentalData.from_csv('X', [r.split(' ') for r in rows], (0,1,None,2))
+    newds = ds.analysis_merge_choices(
+        MockWorker(),
+        ExperimentalData.MergeOptions(
+            track_deferrals_separately=False,
+        ),
+    )
+
+    assert list(newds.export_detailed()) == [
+        ('subjA', 'Ca,Hi,Pa', None, 'Ca,Hi,Pa'),
+        ('subjA', 'Ca,Pa', None, 'Ca,Pa'),
+        ('subjA', 'Ca', None, 'Ca'),
+        None,  # bump progress
+    ]
+
+def test_merging_default_nodef():
+    rows = [
+        'subjA Ca,Hi,Pa Pa Ca',
+        'subjA Ca,Pa Pa Pa',
+        'subjA Ca,Hi,Pa Pa Pa',
+        'subjA Ca Ca Ca',
+        'subjA Ca,Hi,Pa Ca ',
+        'subjA Ca,Pa Pa Ca',
+        'subjA Ca,Pa,Hi Ca Hi',
+        'subjA Ca,Hi,Pa Hi ',
+    ]
+    ds = ExperimentalData.from_csv('X', [r.split(' ') for r in rows], (0,1,2,3))
+    newds = ds.analysis_merge_choices(
+        MockWorker(),
+        ExperimentalData.MergeOptions(
+            track_deferrals_separately=False,
+        ),
+    )
+
+    assert list(newds.export_detailed()) == [
+        ('subjA', 'Ca,Hi,Pa', 'Pa', 'Ca,Pa'),
+        ('subjA', 'Ca,Pa', 'Pa', 'Ca,Pa'),
+        ('subjA', 'Ca', 'Ca', 'Ca'),
+        ('subjA', 'Ca,Hi,Pa', 'Ca', 'Hi'),
+        ('subjA', 'Ca,Hi,Pa', 'Hi', ''),
+        None,  # bump progress
+    ]

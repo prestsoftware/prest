363d56d34fc1334924303ef40d44fb5afc2d90dc
Matus Tejiscak <ziman@functor.sk>
2024-01-27 12:22:49 +0100

    Implement transitivity.

--- prest-lib/src/consistency/stochastic.rs
+++ prest-lib/src/consistency/stochastic.rs
@@ -1,10 +1,12 @@
 use std::result;
-use std::fmt::{self, Display};
-use std::collections::{BTreeMap,HashSet};
-use std::collections::btree_map::Entry;
+use std::fmt;
+use std::collections::HashMap;
 use std::io::{Read,Write};
+use num_rational::Ratio;
+use std::iter::FromIterator;
 
 use alt::Alt;
+use alt_set::AltSet;
 use common::{ChoiceRow,Subject};
 use codec::{self,Encode,Decode,Packed};
 
@@ -71,10 +73,66 @@ struct Transitivity {
     strong : u32,
 }
 
-fn transitivity(_alt_count : u32, _subject : &[ChoiceRow]) -> Transitivity {
+struct MenuStatsRaw {
+    alt_counts : Vec<u32>,
+    total : u32,
+}
+
+type MenuStats = Vec<Ratio<u32>>;
+
+impl MenuStatsRaw {
+    fn finalise(self) -> MenuStats {
+        self.alt_counts.into_iter().map(
+            |num| Ratio::new(num, self.total)
+        ).collect()
+    }
+}
+
+fn transitivity(alt_count : u32, choice_rows : &[ChoiceRow]) -> Transitivity {
     let mut weak = 0;
     let mut moderate = 0;
     let mut strong = 0;
+    const HALF : Ratio<u32> = Ratio::new_raw(1, 2);
+
+    let freq : HashMap<&AltSet, MenuStats> = {
+        let mut freq  : HashMap<&AltSet, MenuStatsRaw> = HashMap::new();
+        for cr in choice_rows {
+            if let Some(choice) = cr.choice.view().as_singleton() {
+                let stats = freq.entry(&cr.menu).or_insert_with(|| MenuStatsRaw {
+                    alt_counts: vec![0; alt_count as usize],
+                    total: 0,
+                });
+
+                stats.alt_counts[choice.index() as usize] += 1;
+                stats.total += 1;
+            }
+        }
+
+        freq.into_iter().map(
+            |(menu, stats)| (menu, stats.finalise())
+        ).collect()
+    };
+
+    for a in Alt::all(alt_count) {
+        for b in Alt::all(alt_count) {
+            if let Some(pab) = freq.get(&AltSet::from_iter([a, b])) {
+                let pa_ab = pab[a.index() as usize];
+                for c in Alt::all(alt_count) {
+                    if let Some(pac) = freq.get(&AltSet::from_iter([a, c])) {
+                        let pa_ac = pac[a.index() as usize];
+                        if let Some(pbc) = freq.get(&AltSet::from_iter([b, c])) {
+                            let pb_bc = pbc[b.index() as usize];
+                            if pa_ab >= HALF && pb_bc >= HALF {
+                                weak += (pa_ac < HALF) as u32;
+                                moderate += (pa_ac < pa_ab && pa_ac < pb_bc) as u32;
+                                strong += (pa_ac < pa_ab || pa_ac < pb_bc) as u32;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     Transitivity {
         weak,
